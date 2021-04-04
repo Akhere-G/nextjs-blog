0f8134a22a29bdfeea89845cad8d505974405a64
Akhere Ihoeghinlan <101akhere5@gmail.com>
2021-04-04 19:42:14 +0100

    added navbar

---
+++ components/Navbar/index.js
@@ -0,0 +1,17 @@
+import { Container, Body } from "./NavbarComponents";
+const links = [
+  { name: "Home", slug: "/" },
+  { name: "Events", slug: "/events" },
+  { name: "About", slug: "/about" },
+  { name: "Contact", slug: "/contact" },
+];
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Body>sss</Body>
+    </Container>
+  );
+};
+
+export default Navbar;
