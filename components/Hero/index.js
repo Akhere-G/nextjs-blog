5377e2ec5ad6426942d77edc21e521af139838c6
Akhere Ihoeghinlan <101akhere5@gmail.com>
2021-04-04 23:25:59 +0100

    added to home page

--- components/Hero/index.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-const Hero = () => {
-  return (
-    <Image width='300px' height='400px' layout='responsive' src='/hero.jpg' />
-  );
-};
-
-export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,51 @@
-import { Hero } from "../components";
 import Head from "next/head";
-
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { cards } from "../data.js";
 export default function Home() {
   return (
     <>
-      <Hero />
+      <article className={styles.hero}>
+        <h2 className={styles.hero_title}>Community</h2>
+        <p className={styles.hero_paragraph}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque
+          aut deserunt architecto expedita aperiam? Exercitationem veritatis
+          suscipit tempora molestiae provident harum aspernatur placeat
+          consequuntur praesentium officia necessitatibus corrupti eos debitis
+          eveniet, deserunt rem corporis quam quasi, numquam similique! Veniam,
+          minus.
+        </p>
+        <p className={styles.hero_paragraph}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque
+          aut deserunt architecto expedita aperiam? Exercitationem veritatis
+          suscipit tempora molestiae provident harum aspernatur placeat
+          consequuntur praesentium officia necessitatibus corrupti eos debitis
+          eveniet, deserunt rem corporis quam quasi, numquam similique! Veniam,
+          minus.
+        </p>
+      </article>
+      <main className={styles.main}>
+        {cards.map((card, index) => {
+          const { src, text } = card;
+          return (
+            <article
+              className={`${styles.card} ${
+                index % 2 === 1 && styles.cardReversed
+              }`}
+            >
+              <Image
+                className={styles.cardImage}
+                src={src}
+                width='300'
+                height='200'
+                fill='responsive
+                '
+              />
+              <p className={styles.cardPara}>{text}</p>
+            </article>
+          );
+        })}
+      </main>
     </>
   );
 }
