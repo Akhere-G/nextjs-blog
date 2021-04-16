b44066f817780991b16428f9dbe1fd0c8b2fae85
Akhere Ihoeghinlan <101akhere5@gmail.com>
2021-04-16 16:54:37 +0100

    added meta info

--- components/Meta/index.js
+++ components/Meta/index.js
@@ -3,6 +3,24 @@ import Head from "next/head";
 const Meta = () => {
   return (
     <Head>
+      <meta charset='UTF-8' />
+      <meta name='viewport' content='width=device-width' />
+      <meta
+        name='description'
+        content='A community based organisation that strives to support our communitry through several programs'
+      />
+      <meta property='og:title' content='Community' />
+      <meta
+        property='og:description'
+        content='A community based organisation that strives to support our communitry through several programs'
+      />
+      <meta property='og:image' content='favicon-32x32.png' />
+      <meta property='og:type' content='website' />
+      <meta name='twitter:card' content='summary' />
+      <meta name='theme-color' content='#020102' />
+      <link rel='icon' href='/favicon.ico' />
+      <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+      <link rel='manifest' href='/site.webmanifest' />
       <title>Community</title>
     </Head>
   );
