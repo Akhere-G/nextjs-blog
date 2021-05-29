35fa86097d3fa38858097ee9402f89485140799c
Akhere Ihoeghinlan <101akhere5@gmail.com>
2021-05-29 18:02:12 +0100

    add bible study page text

--- pages/bible-study/index.js
+++ pages/bible-study/index.js
@@ -3,10 +3,16 @@ import { Hero, Events } from "../../components";
 
 import { createClient } from "contentful";
 
+const paragraphs = [
+  `Amongst other seasonal programs which dive into God’s word, we run a weekly Bible Study. Sessions are twice a week on Thursdays and Sundays at 8pm BST. Our sessions our safe places, where people can learn about God, ask questions, discuss their feelings and thoughts and meet other people of faith.`,
+  `We also have an international, WhatsApp group chat. We have people from all backgrounds and denominations as part of the chat. The group chat is used to pass on words of encouragement, elaborate on the bible study, share music, support one another and so much more.
+  `,
+];
+
 const BibleStudyPage = ({ events }) => {
   return (
     <>
-      <Hero image='volunteer.jpg' />
+      <Hero image='volunteer.jpg' title='Bible Study' paragraphs={paragraphs} />
       <Events program='bible-study' events={events} />
     </>
   );
